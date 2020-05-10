6bd733051551a5b279cbad94b0a3b8a1dec72891
ngangavic <ngangavictor10@gmail.com>
2020-05-10 11:18:24 +0300

    initial project complete

--- app/src/main/java/com/example/databinding/MainActivity.kt
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -1,14 +1,91 @@
 package com.example.databinding
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-class MainActivity : AppCompatActivity() {
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.databinding.model.GroceryItem
+import com.example.databinding.view.GroceryAdapter
+import com.example.databinding.view.NewItemDialogFragment
+import com.example.databinding.viewmodel.GroceryListViewModel
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity(), NewItemDialogFragment.NewItemDialogListener {
+
+    lateinit var viewModel: GroceryListViewModel
+
+    // TODO: remove the view items and change them for the binding object
+    private lateinit var addItemButton: Button
+    private lateinit var groceryListRecyclerView: RecyclerView
+    private lateinit var groceriesTotal: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProviders.of(this).get(GroceryListViewModel::class.java)
 
 
+        // TODO: initialize the binding object and remove the view invocations
+        addItemButton = findViewById(R.id.add_item_button)
+        groceryListRecyclerView = findViewById(R.id.rv_grocery_list)
+        groceriesTotal = findViewById(R.id.total_text_view)
+
+        // TODO: associate the layout manager, adapter and button listener with the binding object
+
+        groceryListRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        groceryListRecyclerView.adapter = GroceryAdapter(viewModel.groceryListItems, this,
+                ::editGroceryItem, ::deleteGroceryItem)
+
+        addItemButton.setOnClickListener {
+            addGroceryItem()
+        }
+
+    }
+
+    override fun onDialogPositiveClick(dialog: DialogFragment, item: GroceryItem, isEdit: Boolean, position: Int?) {
+        if (!isEdit) {
+            viewModel.groceryListItems.add(item)
+        } else {
+            viewModel.updateItem(position!!, item)
+            // TODO: call the adapter from the binding object
+            groceryListRecyclerView.adapter?.notifyDataSetChanged()
+        }
+
+        // TODO: update the total amount and addItemButton with the binding
+        groceriesTotal.text = viewModel.getTotal().toString()
+
+        Snackbar.make(addItemButton, "Item Added Successfully", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+    }
+
+    override fun onDialogNegativeClick(dialog: DialogFragment) {
+        // TODO: update the addItemButton with the binding
+        Snackbar.make(addItemButton, "Nothing Added", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+    }
+
+    private fun addGroceryItem() {
+        val newFragment = NewItemDialogFragment.newInstance(R.string.add_new_item_dialog_title, null)
+        newFragment.show(supportFragmentManager, "newItem")
+    }
+
+    private fun editGroceryItem(position: Int) {
+        Log.d("GoBuy", "edit")
+        val newFragment = NewItemDialogFragment.newInstance(R.string.edit_item_dialog_title,
+                position)
+        newFragment.show(supportFragmentManager, "newItem")
+    }
+
+    private fun deleteGroceryItem(position: Int) {
+        Log.d("GoBuy", "delete")
+        viewModel.removeItem(position)
+        groceriesTotal.text = viewModel.getTotal().toString()
+        // TODO: call the adapter from the binding object
+        groceryListRecyclerView.adapter?.notifyDataSetChanged()
     }
 
 }
